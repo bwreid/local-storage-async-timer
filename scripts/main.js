982efcc92789723d7c7a13cbd2981364e91ce2a1
Wes Reid <bwreid@gmail.com>
2018-03-02 11:52:27 -0800

    Potential solution

--- scripts/main.js
+++ scripts/main.js
@@ -1,4 +1,24 @@
-console.log('JS Loaded!')
-let seconds = 0
+const title = document.querySelector('#title span')
+const buttons = document.querySelectorAll('button')
+const start = buttons[0]
+const stop = buttons[1]
+const parsed = JSON.parse(localStorage.getItem('example'))
+let seconds = parsed ? parsed.seconds : 0
+let timer = null
 
-document.querySelector('#title span').innerHTML = seconds
+title.innerHTML = seconds
+
+start.addEventListener('click', function () {
+  if (timer) return false
+  timer = setInterval(function () {
+    seconds++
+    title.innerHTML = seconds
+    const json = JSON.stringify({ seconds: seconds })
+    localStorage.setItem('example', json)
+  }, 1000)
+})
+
+stop.addEventListener('click', function () {
+  clearInterval(timer)
+  timer = null
+})
